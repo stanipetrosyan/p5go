7f82711d57774575bd797b5904e07239228d3b69
Stanislav Petrosyan <stanislav.petrosyan@cherrychain.it>
2024-05-27 17:15:23 +0200

    added functionality: line

--- window.go
+++ window.go
@@ -26,3 +26,42 @@ func (w *Window) Background(red, green, blue int) {
 	b_float := (1.0 / 255) * float32(blue)
 	gl.ClearColor(r_float, g_float, b_float, 1.0)
 }
+
+func (w *Window) Line(x1, y1, x2, y2 float32) {
+	// TODO: replace with window size
+	x1f2 := ((x1 / 640) * 2) - 1.0
+	y1f2 := ((y1 / 480) * 2) - 1.0
+	x2f2 := ((x2 / 640) * 2) - 1.0
+	y2f2 := ((y2 / 480) * 2) - 1.0
+
+	line := []float32{
+		x1f2, y1f2,
+		x2f2, y2f2,
+	}
+
+	vbo := generatevbo(line)
+	vao := generatevao(vbo)
+
+	gl.BindVertexArray(vao)
+	gl.DrawArrays(gl.LINES, 0, 2)
+}
+
+func generatevbo(array []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 6*len(array), gl.Ptr(array), gl.STATIC_DRAW)
+
+	return vbo
+}
+
+func generatevao(vbo uint32) uint32 {
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 2, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
